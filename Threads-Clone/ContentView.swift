c79c3b4f1301974fdddcf8660686eabb96e7f30c
piwien <berkayyasar01@gmail.com>
2023-11-16 16:36:10 +0300

    Initial Commit

---
+++ Threads-Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threads-Clone
+//
+//  Created by Berkay Yaşar on 16.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threads-Clone/Threads_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_CloneApp.swift
+//  Threads-Clone
+//
+//  Created by Berkay Yaşar on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Threads_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
